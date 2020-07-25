8dc8a0854520e6e6047c6c4bbc0218b9e22e23ef
Ian Mckay <contact@ianmckay.com.au>
2020-07-25 17:25:34 +1000

    Add HCL2 support

--- hcl/hcl.go
+++ hcl/hcl.go
@@ -1,17 +1,23 @@
 package hcl
 
 import (
-	"fmt"
 	"bytes"
 	"encoding/json"
+	"fmt"
+
 	"github.com/hashicorp/hcl"
 	"github.com/hashicorp/hcl/hcl/printer"
+	hcl2 "github.com/hashicorp/hcl/v2/hclsimple"
 )
-func Parse(input string) (string) {
+
+func Parse(input string) string {
 	var ast interface{}
 	err := hcl.Unmarshal([]byte(input), &ast)
 	if err != nil {
-		return fmt.Sprintf("unable to parse JSON: %s", err)
+		err = hcl2.Decode("file.hcl", []byte(input), nil, &ast)
+		if err != nil {
+			return fmt.Sprintf("unable to parse JSON: %s", err)
+		}
 	}
 
 	data, err := json.MarshalIndent(ast, "", "    ")
@@ -22,7 +28,7 @@ func Parse(input string) (string) {
 	return string(data)
 }
 
-func Stringify(input string) (string) {
+func Stringify(input string) string {
 	ast, err := hcl.Parse(input)
 	if err != nil {
 		return fmt.Sprintf("unable to parse HCL: %s", err)
