a1add056a40eb7c039e5d50437607d7f580fb6b7
Ian Mckay <contact@ianmckay.com.au>
2020-08-08 12:58:46 +1000

    Make HCL2 the default

--- hcl/hcl.go
+++ hcl/hcl.go
@@ -7,16 +7,26 @@ import (
 
 	"github.com/hashicorp/hcl"
 	"github.com/hashicorp/hcl/hcl/printer"
-	hcl2 "github.com/hashicorp/hcl/v2/hclsimple"
+
+	hcl2 "github.com/hashicorp/hcl/v2"
+	hcl2syntax "github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
 func Parse(input string) string {
 	var ast interface{}
-	err := hcl.Unmarshal([]byte(input), &ast)
-	if err != nil {
-		err = hcl2.Decode("file.hcl", []byte(input), nil, &ast)
+
+	file, diags := hcl2syntax.ParseConfig([]byte(input), "file.hcl", hcl2.Pos{Line: 1, Column: 1})
+	if diags.HasErrors() {
+		err := hcl.Unmarshal([]byte(input), &ast)
 		if err != nil {
-			return fmt.Sprintf("unable to parse JSON: %s", err)
+			return fmt.Sprintf("unable to parse JSON: %s", diags)
+		}
+	}
+	ast, err := convertFile(file)
+	if err != nil {
+		err := hcl.Unmarshal([]byte(input), &ast)
+		if err != nil {
+			return fmt.Sprintf("unable to convert to JSON: %s", err)
 		}
 	}
 
